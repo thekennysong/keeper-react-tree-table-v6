e363a73e3ead0dda0dd0cb62e7d84a94c8c8f13e
thekennysong <kenny.song1414@gmail.com>
2021-02-24 09:46:15 -0700

    disabled highlight

---
+++ src/component/VirtualListRow.tsx
@@ -0,0 +1,78 @@
+import React, { Component, Children, CSSProperties } from 'react';
+import memoizeOne from 'memoize-one';
+import Column, { ColumnProps } from './Column';
+import CellWrapper from './CellWrapper';
+import TreeState from '../model/tree-state';
+import Row, { RowModel } from '../model/row';
+import { createRow } from '../util/row-creator';
+
+export type VirtualListRowProps = {
+  data: Readonly<TreeState>;
+  model: RowModel;
+  columns: Array<ColumnProps>;
+
+  onChange: (value: Readonly<TreeState>) => void;
+
+  index: number;
+  relIndex: number;
+};
+
+export default class VirtualListRow extends Component<VirtualListRowProps, {}> {
+  render() {
+    //console.log(this.props);
+    const { model, columns, data, index, relIndex } = this.props;
+    const memoed = memoizeOne(createRow);
+    const row: Row = memoed(model, data, this.handleChange);
+
+    return (
+      <div
+        className={`cp_tree-table_row`}
+        style={{
+          ...STYLE_ROW,
+          height: `${row.metadata.height}px`,
+        }}
+        data-index={index}
+        data-relindex={relIndex}
+      >
+        {columns.map((column: ColumnProps, indexKey) => {
+          return (
+            <CellWrapper
+              key={indexKey}
+              indexKey={indexKey}
+              row={row}
+              renderCell={column.renderCell}
+              grow={column.grow}
+              basis={column.basis}
+              columnTotal={columns.length}
+              // background={
+              //   !row.data.isStarReport || row.data.isShow
+              //     ? row.data.isChecked === 'checked'
+              //       ? '#e6f7ff'
+              //       : row.data.isChecked === 'childrenChecked'
+              //       ? 'rgb(230 247 255 / .5)'
+              //       : row.data.upDownColor
+              //       ? row.data.upDownColor
+              //       : ''
+              //     : ''
+              // }
+            />
+          );
+        })}
+      </div>
+    );
+  }
+
+  private handleChange = (value: Readonly<TreeState>): void => {
+    const { onChange } = this.props;
+    onChange(value);
+  };
+}
+
+const STYLE_ROW: CSSProperties = {
+  display: 'flex',
+
+  boxSizing: 'border-box',
+  position: 'relative',
+  overflow: 'hidden',
+  width: '100%',
+};
